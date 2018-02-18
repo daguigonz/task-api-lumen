3c71d1ccc2f716ecdfd7efc43e4502aaebc660fd
Daniel Aguirre <daguigonz.i386@gmail.com>
2018-02-18 17:00:01 -0500

    backend:base api rest

---
+++ task-api/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+// User
+
+$router->post('/users/login', ['uses' => 'UsersController@getToken']);
+$router->group(['middleware' => ['auth']], function () use ($router) {
+    $router->get('/users', ['uses' => 'UsersController@index']);
+    $router->post('/users', ['uses' => 'UsersController@createUser']);
+});
+
+
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('/key', function () {
+    return str_random(32);
+});
+
