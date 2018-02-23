d542c350ce2ec992d2b913a3c9a9f63226cece12
Daniel Aguirre <daguigonz.i386@gmail.com>
2018-02-22 22:54:05 -0500

    app: Se agrego base de la aplicación con VUE.JS

---
+++ octoplus-app/src/appInit.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import routes from './routes'
+
+console.log('%cOCTOPLUS v1 ready! ', 'color: red;font-size:2.0rem;font-weight:bold;');
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      const matchingView = routes[this.currentRoute]
+
+      return matchingView
+        ? require('./pages/' + matchingView + '.vue')
+        : require('./pages/404.vue')
+    }
+  },
+  render (h) {
+    return h(this.ViewComponent)
+  }
+})
+
+window.addEventListener('popstate', () => {
+  app.currentRoute = window.location.pathname
+})
